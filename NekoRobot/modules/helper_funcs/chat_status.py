97c33b76151d3801c9c118be2509ba5a921e0f15
Pruthvi Lalasangi <pruthvilalasangi8@gmail.com>
2024-05-23 17:43:06 +0530

    Update chat_status.py

--- NekoRobot/modules/helper_funcs/chat_status.py
+++ NekoRobot/modules/helper_funcs/chat_status.py
@@ -103,7 +103,7 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
             return user_id in admin_list
 
 
-def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember: None) -> bool:
+def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
     if chat.type == "private" or chat.all_members_are_administrators:
         return True
 
