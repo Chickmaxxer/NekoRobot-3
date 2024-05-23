1166b2e4d53c74f41eaf2fe50d188f04972e27ba
Pruthvi Lalasangi <pruthvilalasangi8@gmail.com>
2024-05-23 16:37:24 +0530

    Update chat_status.py

--- NekoRobot/modules/helper_funcs/chat_status.py
+++ NekoRobot/modules/helper_funcs/chat_status.py
@@ -80,14 +80,11 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
         or user_id in DRAGONS
-        or user_id in DEV_USERS
-        or user_id in WOLVES
-        or user_id in TIGERS
+        or user_id in DEV_USERs
         or chat.all_members_are_administrators
         or user_id in {777000, 1087968824}
     ):  # Count telegram and Group Anonymous as admin
         return True
-   
    if member:
         return member.status in ("administrator", "creator")
 
